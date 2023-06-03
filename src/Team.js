a1c0d7b0ae41248c64500f0a66dd04e9d68c2a86
anya-uwu <anyadev@proton.me>
2023-06-03 11:50:25 -0400

    not sure why this is working but it is

--- src/Team.js
+++ src/Team.js
@@ -15,52 +15,64 @@ const Team = () => {
     // maybe make the generate part a form instead so i can add more selection options later
 
     const [pokemonTeam, setPokemonTeam] = useState([]);
-    const pokemon = [];
-    
-    function random() {
-        let number = Math.floor((Math.random() * 1015) + 1);
-        return number;
-    };
+    const [pokemonTeamTwo, setPokemonTeamTwo] = useState([])
+    const pokemon = []
 
-    function getPokemon(num) {
-        const url = new URL(`https://pokeapi.co/api/v2/pokemon/${num}`);
-        // console.log(url);
-        // url.search = new URLSearchParams({
-        //   name: "bulbasaur", 
-        // });
+    // function random() {
+    //     let number = Math.floor((Math.random() * 1015) + 1);
+    //     return number;
+    // };
 
-        fetch(url)
-            .then(results => {
-                return results.json();
-            }).then(pokeData => {
-                // pokemon.push(pokeData);
-                setPokemonTeam([...pokemonTeam, pokeData])
-                console.log(1)
-                // console.log(pokeData.name);
-            })
-    };
-
-    function generateNums() {
+    function getPokemon() {
         for (let i = 0; i < 6; i++) {
-            getPokemon(random());
-            console.log(2)
+            let num = Math.floor((Math.random() * 1015) + 1);
+
+            const url = new URL(`https://pokeapi.co/api/v2/pokemon/${num}`);
+            // console.log(url);
+            // url.search = new URLSearchParams({
+            //   name: "bulbasaur", 
+            // });
+
+            fetch(url)
+                .then(results => {
+                    return results.json();
+                }).then(pokeData => {
+                    pokemon.push(pokeData);
+                    // setPokemonTeamTwo(pokemon);
+                    setPokemonTeam([...pokemonTeam, pokeData]);
+                    // console.log(pokeData.name);
+                })
         }
+
     };
 
-    
+    // function generateNums() {
+    //     for (let i = 0; i < 6; i++) {
+    //         getPokemon();
+    //         console.log(2)
+    //     }
+    // };
+
+    function click() {
+        console.log(3)
+        getPokemon();
+        setPokemonTeamTwo(pokemon);
+
+    }
+
     return (
         <section className="flexContainer">
-            {/* <button onClick={generateNums}>Generate</button> */}
+            <button onClick={click}>Generate</button>
             {/* <Form handleSubmit={generateNums}/> */}
             <ul className="team flexContainer">
                 {
-                    pokemonTeam.map((pokemonObj) => {
-                        return(
-                        <Pokemon
-                            key={pokemonObj.id}
-                            name={pokemonObj.name}
-                            type={pokemonObj.types[0].type.name}
-                            imageSource={pokemonObj.sprites.front_default}/>
+                    pokemonTeamTwo.map((pokemonObj) => {
+                        return (
+                            <Pokemon
+                                key={pokemonObj.id}
+                                name={pokemonObj.name}
+                                type={pokemonObj.types[0].type.name}
+                                imageSource={pokemonObj.sprites.front_default} />
                         );
                     })
                 }
