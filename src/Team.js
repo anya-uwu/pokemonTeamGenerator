b46ada8f4e401b7b1019e2e18f8b6068c3066dba
anya-uwu <anyadev@proton.me>
2023-06-03 14:13:32 -0400

    used setTimeout function to make sure we have the pokemon before we set state

--- src/Team.js
+++ src/Team.js
@@ -14,8 +14,9 @@ const Team = () => {
 
     // maybe make the generate part a form instead so i can add more selection options later
 
-    const [pokemonTeam, setPokemonTeam] = useState([]);
+    // const [pokemonTeam, setPokemonTeam] = useState([]);
     const [pokemonTeamTwo, setPokemonTeamTwo] = useState([])
+
     const pokemon = []
 
     // function random() {
@@ -26,6 +27,7 @@ const Team = () => {
     function getPokemon() {
         for (let i = 0; i < 6; i++) {
             let num = Math.floor((Math.random() * 1015) + 1);
+            console.log(num)
 
             const url = new URL(`https://pokeapi.co/api/v2/pokemon/${num}`);
             // console.log(url);
@@ -38,12 +40,16 @@ const Team = () => {
                     return results.json();
                 }).then(pokeData => {
                     pokemon.push(pokeData);
+
                     // setPokemonTeamTwo(pokemon);
-                    setPokemonTeam([...pokemonTeam, pokeData]);
+                    // setPokemonTeam(...pokemonTeam, pokeData);
+                    // console.log(pokemonTeam)
+                    
                     // console.log(pokeData.name);
                 })
+            
         }
-
+        // setPokemonTeamTwo(pokemon);
     };
 
     // function generateNums() {
@@ -54,10 +60,11 @@ const Team = () => {
     // };
 
     function click() {
-        console.log(3)
-        getPokemon();
-        setPokemonTeamTwo(pokemon);
-
+        getPokemon()
+        setTimeout(() => {
+            setPokemonTeamTwo(pokemon)
+        }, 100)
+        
     }
 
     return (
@@ -66,6 +73,7 @@ const Team = () => {
             {/* <Form handleSubmit={generateNums}/> */}
             <ul className="team flexContainer">
                 {
+                    // pokemonTeamTwo.length > 0 ? 
                     pokemonTeamTwo.map((pokemonObj) => {
                         return (
                             <Pokemon
@@ -73,8 +81,21 @@ const Team = () => {
                                 name={pokemonObj.name}
                                 type={pokemonObj.types[0].type.name}
                                 imageSource={pokemonObj.sprites.front_default} />
+
+                            // <li className="pokemon flexContainer">
+                            //     <div className="imageContainer">
+                            //         <img src={pokemonObj.sprites.front_default} alt={`image of ${pokemonObj.name}`} />
+                            //     </div>
+                            //     <div className="textContainer">
+                            //         <h2>{pokemonObj.name}</h2>
+
+                            //         {/* could also be a p element */}
+                            //         <h3>{pokemonObj.types[0].type.name}</h3>
+                            //     </div>
+                            // </li>
                         );
                     })
+                    // : null
                 }
             </ul>
         </section>
