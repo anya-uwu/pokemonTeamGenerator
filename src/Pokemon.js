392467c4058bfee02b27e0c3d5672bd244377f90
anya-uwu <anyadev@proton.me>
2023-06-04 11:52:00 -0400

    working on styling

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -5,13 +5,12 @@ const Pokemon = ({ name, type, type2, imageSource }) => {
     return(
         <li className="pokemon flexContainer">
             <div className="imageContainer">
-                <img src={imageSource} alt={`image of ${name}`} />
+                <img src={imageSource} alt={`image of the pokemon ${name}`} />
             </div>
             <div className="textContainer">
                 <h2>{name}</h2>
             
-                <h3>{type}</h3> 
-                <h3>{type2}</h3>
+                <h3>{type} {type2}</h3> 
             </div>
         </li>
     )
