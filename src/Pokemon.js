a444a6545107a155db3aa40334487c431686faa0
anya-uwu <anyadev@proton.me>
2023-06-04 21:22:39 -0400

    added some comments

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -7,7 +7,7 @@ const Pokemon = ({ name, type, type2, imageSource }) => {
     return(
         <li className="pokemon flexContainer">
             <div className="imageContainer">
-                <img src={imageSource} alt={`image of the pokemon ${name}`} />
+                <img src={imageSource} alt={`The pokemon ${name}`} />
             </div>
             <div className="textContainer">
                 <h2>{name}</h2>
